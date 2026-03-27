3a6b49613eeaa01495eb64c8f80d9693846a400d
MilansometimesCodes <mpurushotam@stu.ncu.edu.jm>
2026-03-27 00:30:22 -0500

    logout.php

--- StudySync_week3_B/Code/Code/logout.php
+++ StudySync_week3_B/Code/Code/logout.php
@@ -1,12 +1,44 @@
 <?php
 require_once 'config.php';
 
-$_SESSION = [];
+if (isset($_GET['confirm']) && $_GET['confirm'] === 'yes') {
+    $_SESSION = [];
 
-if (ini_get('session.use_cookies')) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    if (ini_get('session.use_cookies')) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params['path'],
+            $params['domain'],
+            $params['secure'],
+            $params['httponly']
+        );
+    }
+
+    session_destroy();
+    redirectTo('login.php');
+    exit();
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Logout</title>
+<script>
+    window.onload = function () {
+        var answer = confirm("Are you sure you want to log out?");
 
-session_destroy();
-redirectTo('login.php');
+        if (answer) {
+            window.location.href = "logout.php?confirm=yes";
+        } else {
+            window.location.href = "dashboard.php";
+        }
+    };
+</script>
+</head>
+<body>
+</body>
+</html>
